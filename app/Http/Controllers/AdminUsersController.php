39d1a513cfb43cc9cc2bf06f6b60a3aff14a846d
buna_branislav <branislav.buna@spiressl.com>
2020-04-05 13:22:44 +0100

    updating user

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -6,6 +6,7 @@ use App\Role;
 use App\Photo;
 use Illuminate\Http\Request;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEditRequest;
 
 class AdminUsersController extends Controller
 {
@@ -80,7 +81,10 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $roles = Role::pluck('name', 'id')->all();
+
+        return view('admin.users.edit', compact('user', 'roles'));
     }
 
     /**
@@ -90,9 +94,25 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+        $input = $request->all();
+
+        if($file = $request->file('photo_id')){
+
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file' => $name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -22,7 +22,7 @@
                     <tr>
                         <td scope="row">{{$user->id}}</td>
                         <td><img height="50" src="{{$user->photo ? $user->photo->file : 'User has no photo!'}}" alt=""/></td>
-                        <td>{{$user->name}}</td>
+                        <td><a href="{{route('users.edit', $user->id)}}">{{$user->name}}</a></td>
                         <td>{{$user->email}}</td>
                         <td>{{$user->role ? $user->role->name : 'User has no role!'}}</td>
                         <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
